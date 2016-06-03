28c9d0d4b065108de27f91983d04ab14b7bb9fd8
Markus Felten <Markus.Felten@gmx.de>
2016-06-03 21:10:55 +0200

    test: 1st. test

---
+++ tests/simple_test.js
@@ -0,0 +1,25 @@
+/* global describe, it*/
+/* jslint node: true, esnext: true */
+'use strict';
+
+const chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should(),
+  endpoint = require('kronos-endpoint'),
+  service = require('kronos-service'),
+  ServiceConfig = service.ServiceConfig,
+  GelfLogger = require('../service.js');
+
+class ServiceProvider extends service.ServiceProviderMixin(service.Service) {}
+
+const sp = new ServiceProvider();
+
+describe('gelf logger service', () => {
+  it('got state response', () =>
+    GelfLogger.registerWithManager(sp).then(() => {
+      const logger = sp.services.logger;
+      console.log(logger);
+      return logger.start().then(() => assert.equal(logger.state, 'running'));
+    }));
+});
