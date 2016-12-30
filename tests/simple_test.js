00f9499c6eb7570caa8b566914c512983795ed24
Markus Felten <Markus.Felten@gmx.de>
2016-12-30 01:40:22 +0100

    feat: provide es6 module

--- tests/simple_test.js
+++ tests/simple_test.js
@@ -6,18 +6,20 @@ const chai = require('chai'),
   assert = chai.assert,
   expect = chai.expect,
   should = chai.should(),
-  endpoint = require('kronos-endpoint'),
-  service = require('kronos-service'),
-  ServiceConfig = service.ServiceConfig,
-  GelfLogger = require('../service.js');
+  {
+    Service, ServiceProviderMixin
+  } = require('kronos-service'),
+  {
+    GelfLoggerService, registerWithManager
+  } = require('../dist/module.js');
 
-class ServiceProvider extends service.ServiceProviderMixin(service.Service) {}
+class ServiceProvider extends ServiceProviderMixin(Service) {}
 
 const sp = new ServiceProvider();
 
 describe('gelf logger service', () => {
   it('got state response', () =>
-    GelfLogger.registerWithManager(sp).then(() => {
+    registerWithManager(sp).then(() => {
       const logger = sp.services.logger;
       return logger.start().then(() => assert.equal(logger.state, 'running'));
     }));
