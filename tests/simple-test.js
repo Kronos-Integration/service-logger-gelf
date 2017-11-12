0e469ffcbd5cd69adc62649295766c559047fa73
Markus Felten <Markus.Felten@gmx.de>
2017-11-12 22:33:25 +0100

    chore: avarization

--- tests/simple-test.js
+++ tests/simple-test.js
@@ -1,26 +1,20 @@
-/* global describe, it*/
-/* jslint node: true, esnext: true */
-'use strict';
+import { Service, ServiceProviderMixin } from 'kronos-service';
+import {
+  GelfLoggerService,
+  registerWithManager
+} from '../src/gelf-logger-service';
 
-const chai = require('chai'),
-  assert = chai.assert,
-  expect = chai.expect,
-  should = chai.should(),
-  {
-    Service, ServiceProviderMixin
-  } = require('kronos-service'),
-  {
-    GelfLoggerService, registerWithManager
-  } = require('../dist/module.js');
+import test from 'ava';
 
 class ServiceProvider extends ServiceProviderMixin(Service) {}
 
 const sp = new ServiceProvider();
 
-describe('gelf logger service', () => {
-  it('got state response', () =>
-    registerWithManager(sp).then(() => {
-      const logger = sp.services.logger;
-      return logger.start().then(() => assert.equal(logger.state, 'running'));
-    }));
+test('gelf logger service state response', async t => {
+  await registerWithManager(sp);
+
+  const logger = sp.services.logger;
+  await logger.start();
+
+  t.is(logger.state, 'running');
 });
