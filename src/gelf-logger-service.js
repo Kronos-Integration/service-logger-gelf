d693d082d925ffbef554c8257fdfb6749bfe724a
Markus Felten <Markus.Felten@gmx.de>
2017-12-23 17:34:59 +0100

    fix: use async/await

--- src/gelf-logger-service.js
+++ src/gelf-logger-service.js
@@ -1,93 +1,83 @@
-/* jslint node: true, esnext: true */
-
-'use strict';
-
-import {
-	Service,
-	ServiceLogger
-}
-from 'kronos-service';
-import {
-	createAttributes, mergeAttributes
-}
-from 'model-attributes';
+import { Service, ServiceLogger } from 'kronos-service';
+import { createAttributes, mergeAttributes } from 'model-attributes';
 
 const Gelf = require('gelf');
 
 /**
  * Log receiving service
  */
-class GelfLoggerService extends ServiceLogger {
+export class GelfLoggerService extends ServiceLogger {
+  /**
+   * @return {string} 'gelf-logger'
+   */
+  static get name() {
+    return 'gelf-logger';
+  }
 
-	static get name() {
-		return 'gelf-logger';
-	}
+  static get configurationAttributes() {
+    return mergeAttributes(
+      createAttributes({
+        graylogPort: {
+          description: 'gelf server port',
+          type: 'ip-port',
+          default: 12201,
+          needsRestart: true
+        },
+        graylogHostname: {
+          description: 'gelf server name',
+          type: 'hostname',
+          default: '127.0.0.1',
+          needsRestart: true
+        },
+        connection: {
+          type: 'string',
+          default: 'wan',
+          needsRestart: true
+        },
+        maxChunkSizeWan: {
+          type: 'unsigned-integer',
+          default: 1420,
+          needsRestart: true
+        },
+        maxChunkSizeLan: {
+          type: 'unsigned-integer',
+          default: 8154,
+          needsRestart: true
+        }
+      }),
+      Service.configurationAttributes
+    );
+  }
 
-	static get configurationAttributes() {
-		return mergeAttributes(createAttributes({
-			graylogPort: {
-				description: 'gelf server port',
-				type: 'ip-port',
-				default: 12201,
-				needsRestart: true
-			},
-			graylogHostname: {
-				description: 'gelf server name',
-				type: 'hostname',
-				default: '127.0.0.1',
-				needsRestart: true
-			},
-			connection: {
-				type: 'string',
-				default: 'wan',
-				needsRestart: true
-			},
-			maxChunkSizeWan: {
-				type: 'unsigned-integer',
-				default: 1420,
-				needsRestart: true
-			},
-			maxChunkSizeLan: {
-				type: 'unsigned-integer',
-				default: 8154,
-				needsRestart: true
-			}
-		}), Service.configurationAttributes);
-	}
+  async _start() {
+    const gelf = new Gelf({
+      graylogPort: this.graylogPort,
+      graylogHostname: this.graylogHostname,
+      connection: this.connection,
+      maxChunkSizeWan: this.maxChunkSizeWan,
+      maxChunkSizeLan: this.maxChunkSizeLan
+    });
 
-	_start() {
-		const gelf = new Gelf({
-			graylogPort: this.graylogPort,
-			graylogHostname: this.graylogHostname,
-			connection: this.connection,
-			maxChunkSizeWan: this.maxChunkSizeWan,
-			maxChunkSizeLan: this.maxChunkSizeLan
-		});
-
-		this.endpoints.log.receive = entry => {
-			try {
-				gelf.emit('gelf.log', entry);
-			} catch (e) {
-				console.error(`Unable to log entry with the following keys: ${Object.keys(entry)} ${e}`);
-				return Promise.reject(e);
-			}
-			return Promise.resolve();
-		};
-
-		return Promise.resolve();
-	}
+    this.endpoints.log.receive = async entry => {
+      try {
+        gelf.emit('gelf.log', entry);
+      } catch (e) {
+        console.error(
+          `Unable to log entry with the following keys: ${Object.keys(
+            entry
+          )} ${e}`
+        );
+        throw e;
+      }
+    };
+  }
 }
 
-function registerWithManager(manager) {
-	return manager.registerServiceFactory(GelfLoggerService).then(sf => {
-		return manager.declareService({
-			type: GelfLoggerService.name,
-			name: 'logger'
-		});
-	});
+export function registerWithManager(manager) {
+  return manager.registerServiceFactory(GelfLoggerService).then(sf =>
+    manager.declareService({
+      type: GelfLoggerService.name,
+      name: 'logger'
+    })
+  );
 }
-
-export {
-	GelfLoggerService,
-	registerWithManager
-};
