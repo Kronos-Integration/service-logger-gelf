8155aff1795269e6f6746ad01cb96df02847d933
Markus Felten <Markus.Felten@gmx.de>
2025-07-22 18:37:03 +0100

    feat: use pacc

--- src/gelf-logger-service.mjs
+++ src/gelf-logger-service.mjs
@@ -1,7 +1,6 @@
-import { Service, ServiceLogger } from '@kronos-integration/service';
-import { createAttributes, mergeAttributes } from 'model-attributes';
-import Gelf from 'gelf';
-
+import { Service, ServiceLogger } from "@kronos-integration/service";
+import { prepareAttributesDefinitions, default_attribute } from "pacc";
+import Gelf from "gelf";
 
 /**
  * Log receiving service
@@ -11,43 +10,42 @@ export class GelfLoggerService extends ServiceLogger {
    * @return {string} 'gelf-logger'
    */
   static get name() {
-    return 'gelf-logger';
+    return "gelf-logger";
   }
 
-  static get configurationAttributes() {
-    return mergeAttributes(
-      createAttributes({
-        graylogPort: {
-          description: 'gelf server port',
-          type: 'ip-port',
-          default: 12201,
-          needsRestart: true
-        },
-        graylogHostname: {
-          description: 'gelf server name',
-          type: 'hostname',
-          default: '127.0.0.1',
-          needsRestart: true
-        },
-        connection: {
-          type: 'string',
-          default: 'wan',
-          needsRestart: true
-        },
-        maxChunkSizeWan: {
-          type: 'unsigned-integer',
-          default: 1420,
-          needsRestart: true
-        },
-        maxChunkSizeLan: {
-          type: 'unsigned-integer',
-          default: 8154,
-          needsRestart: true
-        }
-      }),
-      Service.configurationAttributes
-    );
-  }
+  static attributes = prepareAttributesDefinitions(
+    {
+      graylogPort: {
+        description: "gelf server port",
+        type: "ip-port",
+        default: 12201,
+        needsRestart: true
+      },
+      graylogHostname: {
+        ...default_attribute,
+        description: "gelf server name",
+        type: "hostname",
+        default: "127.0.0.1",
+        needsRestart: true
+      },
+      connection: {
+        ...default_attribute,
+        default: "wan",
+        needsRestart: true
+      },
+      maxChunkSizeWan: {
+        type: "unsigned-integer",
+        default: 1420,
+        needsRestart: true
+      },
+      maxChunkSizeLan: {
+        type: "unsigned-integer",
+        default: 8154,
+        needsRestart: true
+      }
+    },
+    Service.attributes
+  );
 
   async _start() {
     const gelf = new Gelf({
@@ -60,7 +58,7 @@ export class GelfLoggerService extends ServiceLogger {
 
     this.endpoints.log.receive = async entry => {
       try {
-        gelf.emit('gelf.log', entry);
+        gelf.emit("gelf.log", entry);
       } catch (e) {
         console.error(
           `Unable to log entry with the following keys: ${Object.keys(
